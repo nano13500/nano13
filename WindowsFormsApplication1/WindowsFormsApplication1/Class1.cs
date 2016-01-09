ed3a00b9af03b4322e5ad2e149d6accc71f472f9
nano13500 <leciel009@gmail.com>
2016-01-10 00:32:05 +0100

    Hello

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        static string hello(string toto)
+        {
+            return toto;
+        }
+    }
+}
